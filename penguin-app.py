caec7be5e0888442cd466c12250c86bd0f7ea631
Divyanshu-12 <71869590+Divyanshu-12@users.noreply.github.com>
2020-10-06 20:41:55 +0530

    Add files via upload

--- penguin-app.py
+++ penguin-app.py
@@ -4,6 +4,7 @@ import numpy as np
 import pickle
 import shap
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA
 
 import matplotlib.pyplot as plt
 st.write("""
@@ -12,24 +13,24 @@ This app predicts the **Palmer Penguin** species!
 Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
 """)
 
-st.sidebar.header('User Input Features')
+st.header('User Input Features')
 st.set_option('deprecation.showfileUploaderEncoding', False)
-st.sidebar.markdown("""
+st.markdown("""
 [Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
 """)
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+uploaded_file = st.file_uploader("Upload your input CSV file", type=["csv"])
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
     def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-        sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_kg = st.sidebar.slider('Body mass (kg)', 2.7,3.9,4.2)
+        island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+        sex = st.selectbox('Sex',('male','female'))
+        bill_length_mm = st.slider('Bill length (mm)', 32.1,59.6,43.9)
+        bill_depth_mm = st.slider('Bill depth (mm)', 13.1,21.5,17.2)
+        flipper_length_mm = st.slider('Flipper length (mm)', 172.0,231.0,201.0)
+        body_mass_kg = st.slider('Body mass (kg)', 2.7,3.9,3.0)
         data = {'island': island,
                 'bill_length_mm': bill_length_mm,
                 'bill_depth_mm': bill_depth_mm,
@@ -71,13 +72,19 @@ st.write(penguins_species[prediction])
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
 
+
+
+
+
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 explainer = shap.TreeExplainer(load_clf)
 shap_values = explainer.shap_values(df)
 
 st.header('Feature Importance')
-plt.title('Feature importance based on SHAP values')
+plt.title('Feature importance based on SHAP values(BAR)')
 shap.summary_plot(shap_values, df,plot_type="bar")
 st.pyplot(bbox_inches='tight')
 st.write('---')
+
